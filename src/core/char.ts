4f88b9259899f69db896db8aff04cddab78fd809
Ronald Dijks <ronalddijks@protonmail.com>
2025-05-11 20:50:23 +0200

    Add GitHub Action to run tests with coverage

--- src/core/char.ts
+++ src/core/char.ts
@@ -6,10 +6,6 @@ export const isAlpha = (c: string): boolean => {
   return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z");
 };
 
-export const isAlphanumeric = (c: string): boolean => {
-  return isAlpha(c) || isNumeric(c);
-};
-
 export const isWhitespace = (c: string): boolean => {
   return c === " " || c === "\t" || c === "\n";
 };
